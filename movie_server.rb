ded761c3e77c1b417a86971828d93dc5630a0426
will love <wlove29@gmail.com>
2015-03-27 17:16:44 -0400

    movie view is now showing year and name

--- movie_server.rb
+++ movie_server.rb
@@ -2,11 +2,23 @@ require 'imdb'
 require 'sinatra'
 require 'pry'
 
+class Movie
+	def search(word)
+      searched_movies = Imdb::Search.new(word).movies
+      random_movies = searched_movies.first(30)
+      movies = random_movies.map{|m|
+        {title: m.title,poster: m.poster}
+      }
+  	end
+end
+
 get '/' do
 erb :movies 
 end
 
-post '/movies' do 
-@i = Imdb::Search.new(params[:movie])
-erb :list_movies	
+post '/list_movies' do
+  @movies = Movie.new.search(params[:movie])
+  erb :list_movies
 end
+
+*
